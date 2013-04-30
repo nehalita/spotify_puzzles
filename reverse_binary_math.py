21d354f5496f0295760df153801af52e3d238f9b
Neha Batra <nehalita@gmail.com>
2013-04-30 16:44:55 -0400

    Changed reverse_binary files to take input from sys.stdin.

--- reverse_binary_math.py
+++ reverse_binary_math.py
@@ -1,4 +1,5 @@
 import math
+import sys
 
 def get_binary_from_num(num_input, bin_num=0):
     if num_input == 0:
@@ -23,18 +24,23 @@ def reverse_binary(num_input):
     rev_bin_num = get_number_from_bin(rev_num)
     return rev_bin_num
 
-assert get_binary_from_num(0) == 0
-assert get_binary_from_num(1) == 1
-assert get_binary_from_num(2) == 10
-assert get_binary_from_num(3) == 11
-assert get_binary_from_num(4) == 100
-assert get_binary_from_num(5) == 101
-assert get_binary_from_num(13) == 1101
-assert get_binary_from_num(133) == 10000101
+def test_binary_program():
+    assert get_binary_from_num(0) == 0
+    assert get_binary_from_num(1) == 1
+    assert get_binary_from_num(2) == 10
+    assert get_binary_from_num(3) == 11
+    assert get_binary_from_num(4) == 100
+    assert get_binary_from_num(5) == 101
+    assert get_binary_from_num(13) == 1101
+    assert get_binary_from_num(133) == 10000101
 
-assert reverse_number(4321) == 1234
+    assert reverse_number(4321) == 1234
 
-assert get_number_from_bin(10000101) == 133
+    assert get_number_from_bin(10000101) == 133
 
-assert reverse_binary(13) == 11
-assert reverse_binary(47) == 61
+    assert reverse_binary(13) == 11
+    assert reverse_binary(47) == 61
+
+if __name__ == '__main__':
+    input_int = int(sys.stdin.read())
+    print get_binary_from_num(input_int)
